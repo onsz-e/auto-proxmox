4448bd7056308c4ff2c58d946926a80d6ebd59f3
onsz-e <jonasz.jozwicki@gmail.com>
2026-01-16 19:54:23 +0100

    Fixed the read issue.

--- init.sh
+++ init.sh
@@ -2,11 +2,27 @@
 
 set -e
 
-read OPT < /dev/tty
-echo "$OPT"
+menu() {
+    echo """
+###HELLO###
+For more information about each option head to: 
+- 
+###MENU###
+1) Basic PVE configuration.
 
-# TMP_SCRIPT=$(mktemp)
-# echo 'curl -sSL https://raw.githubusercontent.com/onsz-e/auto-proxmox/main/src/start.sh' >> "$TMP_SCRIPT"
-# chmod +x "$TMP_SCRIPT"
-# bash "$TMP_SCRIPT" < /dev/tty
-# rm -f "$TMP_SCRIPT"
+What do you want to do?
+"""
+
+    read OPTION < /dev/tty
+
+    case "$OPTION" in
+    1)
+        curl -sSL https://raw.githubusercontent.com/onsz-e/auto-proxmox/main/src/basic-pve-configuration.sh
+        ;;
+    *)
+        echo "There is no such option."
+        ;;
+    esac
+}
+
+menu
