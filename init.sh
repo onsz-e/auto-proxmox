5af5d32bea857545e6fbd42fdc8e185c6ad0d73a
onsz-e <jonasz.jozwicki@gmail.com>
2026-01-16 19:24:37 +0100

    Fixed wrong repo name in init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 set -e
 
 TMP_SCRIPT=$(mktemp)
-curl -sSL https://raw.githubusercontent.com/onsz-e/proxmox-configuration/main/src/start.sh -o "$TMP_SCRIPT"
+curl -sSL https://raw.githubusercontent.com/onsz-e/auto-proxmox/main/src/start.sh -o "$TMP_SCRIPT"
 chmod +x "$TMP_SCRIPT"
 bash "$TMP_SCRIPT"
 rm -f "$TMP_SCRIPT"
