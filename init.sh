76ca0424c077651f5a91c57601386995a55cab45
onsz-e <jonasz.jozwicki@gmail.com>
2026-01-16 19:10:58 +0100

    Changes to the init script.

--- init.sh
+++ init.sh
@@ -9,18 +9,22 @@ For more information about each option head to:
 - 
 ###MENU###
 1) Basic PVE configuration.
+
+What do you want to do?
 """
 
     read OPTION
 
     case "$OPTION" in
     1)
-        curl -sSL https://raw.githubusercontent.com/onsz-e/proxmox-configuration/main/src/basic-pve-configuration.sh | bash;
-        menu
+        TMP_SCRIPT=$(mktemp)
+        curl -sSL https://raw.githubusercontent.com/onsz-e/proxmox-configuration/main/src/basic-pve-configuration.sh -o "$TMP_SCRIPT"
+        chmod +x "$TMP_SCRIPT"
+        bash "$TMP_SCRIPT"
+        rm -f "$TMP_SCRIPT"
         ;;
     *)
-        echo "There is no such option.";
-        menu
+        echo "There is no such option."
         ;;
     esac
 }
