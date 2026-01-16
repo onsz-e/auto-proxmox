f14a4585e228e4aef71e386b018af64c3fbe0a01
onsz-e <jonasz.jozwicki@gmail.com>
2026-01-16 19:46:03 +0100

    Debbuging.

--- init.sh
+++ init.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-TMP_SCRIPT=$(mktemp)
-echo 'curl -sSL https://raw.githubusercontent.com/onsz-e/auto-proxmox/main/src/start.sh' >> "$TMP_SCRIPT"
-chmod +x "$TMP_SCRIPT"
-bash "$TMP_SCRIPT" < /dev/tty
-rm -f "$TMP_SCRIPT"
+read OPT < /dev/tty
+echo "$OPT"
+
+# TMP_SCRIPT=$(mktemp)
+# echo 'curl -sSL https://raw.githubusercontent.com/onsz-e/auto-proxmox/main/src/start.sh' >> "$TMP_SCRIPT"
+# chmod +x "$TMP_SCRIPT"
+# bash "$TMP_SCRIPT" < /dev/tty
+# rm -f "$TMP_SCRIPT"
